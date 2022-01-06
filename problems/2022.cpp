4e705d52c264a94d99052b6ec49c82a0782730a9
Neeraj Sujan <nrj127@gmail.com>
2022-01-06 19:07:38 +0100

    optimized approach 2022 problem

--- problems/2022.cpp
+++ problems/2022.cpp
@@ -20,11 +20,29 @@
         return result;
     }
     
+vector<vector<int>> optimized(vector<int>& original, int m, int n) {
+        if(m*n != original.size()) {
+        return vector<vector<int>>();
+    }
+        
+        vector<vector<int>> result(m, std::vector<int>(n));
+        
+        for(int k = 0; k< original.size(); k++) {
+            int i = k/n;
+            int j = k%n;
+            
+            result[i][j] = original[k];
+            
+        }
+        
+        return result;
+    }
+    
 int main() {
     std::vector<int> original {1,2,3};
     int m = 1;
     int n = 3;
-    auto result = construct2DArray(original, m, n);
+    auto result = optimized(original, m, n);
     
     for(int i = 0; i < m; i++) {
        for(int j = 0; j < n; j++) {
