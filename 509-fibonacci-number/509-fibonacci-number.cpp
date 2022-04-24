58c5884f7484be44d883f475cb30ec3ab98df96c
Neeraj Sujan <29118597+sujanneeraj@users.noreply.github.com>
2022-04-24 10:02:08 +0200

    Time: 7 ms (38.14%), Space: 6.2 MB (10.53%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,12 +2,29 @@ class Solution {
 public:
     int fib(int n) {
         
-       return recursiveApproach(n);
+    //   return recursiveApproach(n);
+        return optimizedApproach(n);
         
     }
     
     
 private:
+    
+   int optimizedApproach(int n) {
+       if(n<=1) {
+           return n;
+       }
+       vector<int> dp(n+1, 0);
+       dp[0] = 0;
+       dp[1] = 1;
+       for(int i = 2; i <dp.size(); i++) {
+           dp[i] = dp[i-1] + dp[i-2];
+       }
+       
+       return dp[dp.size()-1];
+   } 
+    
+    
   int  recursiveApproach(int n) {
         if(n <= 1) {
             return n;
