689b6078b5928f3366693209a4dfe77c72315fb1
Neeraj Sujan <29118597+sujanneeraj@users.noreply.github.com>
2022-07-11 22:30:52 +0200

    Time: 26 ms (44.97%), Space: 19.8 MB (55.19%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -2,11 +2,43 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         
-        return Soln2(height);
+        return Soln3(height);
         
     }
     
 private:
+    
+    int Soln3(vector<int>& height){
+        
+        
+        if(height.size() == 0) {
+            return 0;
+        }
+        
+        int left = 0;
+        int right = height.size() - 1;
+        int result = 0;
+        
+        int leftMax = height[left];
+        int rightMax = height[right];
+        
+        while(left < right) {
+            
+            if(leftMax < rightMax) {
+                left +=1;
+                leftMax = max(leftMax, height[left]);
+                result += leftMax - height[left];
+            }
+            else {
+                right -=1;
+                rightMax = max(rightMax, height[right]);
+                result += rightMax - height[right];
+            }
+        }
+        
+        return result;
+        
+    }
     
     int Soln2(vector<int>& height) {
         vector<int> leftMax(height.size(),0);
