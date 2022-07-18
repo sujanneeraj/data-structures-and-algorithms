c2f0cc95f56a9089274a2ba7551098ef1fe65d7e
Neeraj Sujan <29118597+sujanneeraj@users.noreply.github.com>
2022-07-18 17:07:00 +0200

    Time: 738 ms (5.00%), Space: 19.8 MB (60.78%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
@@ -1,17 +1,26 @@
 class KthLargest {
+    // keep only k largest elements in descending order
     vector<int> container;
     int k;
 public:
     KthLargest(int k, vector<int>& nums) {
-        container = nums;
-        sort(container.begin(), container.end());
         this->k = k;
+        for(int &val: nums) {
+             if(container.size() < k || container.empty() || val > container.back())
+                insert(val);
+        }
+        
     }
     
     int add(int val) {
-        return approachOne(val);
+        if(container.size() < k  || container.empty() || val > container.back()) {
+            insert(val);
+        }
+        
+        return container.back();
     }
 private:
+    /*
     int approachOne(int val){
         container.push_back(val);
         int idx = container.size() - 1;
@@ -21,6 +30,23 @@ private:
         }
         container[idx] = val;
         return container[container.size() -k];
+    }
+    */
+    void insert(int val) {
+        if(container.size() == k) {
+            
+            
+            container.pop_back();
+            
+        }
+        container.push_back(val);
+        int idx = container.size() - 1;
+        while(idx > 0 && container[idx-1] < val) {
+            container[idx] = container[idx-1];
+            --idx;
+        }
+        container[idx] = val;
+        
     }
 };
 
